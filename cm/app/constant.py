adbdf8be2aab85a8d25acd39f45b8b5004df158f
Gwenaëlle Gustin <gustin.gwenaelle96@gmail.com>
2024-11-19 15:10:41 +0100

    FEAT: real example of description

--- cm/app/constant.py
+++ cm/app/constant.py
@@ -23,7 +23,7 @@ TRANFER_PROTOCOLE ='http://'
 INPUTS_CALCULATION_MODULE = [
     {'input_name': 'Multiplication factor',
      'input_type': 'input',
-     'input_description': 'Description of the input',
+     'input_description': 'This factor will multiply the layer values',
      'input_parameter_name': 'multiplication_factor',
      'input_value': '1',
      'input_priority': 0,
